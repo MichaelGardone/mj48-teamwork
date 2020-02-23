36b7e227368323511fe85a7adae9f09638ebe8ec
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-02-23 14:05:02 -0700

    better control script naming

--- MJ48-Teamwork/Assets/Scripts/FX/ButtonSwitchAnim.cs
+++ MJ48-Teamwork/Assets/Scripts/FX/ButtonSwitchAnim.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ButtonSwitch : MonoBehaviour
+public class ButtonSwitchAnim : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] Sprite Button_Down;
