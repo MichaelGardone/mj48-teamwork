48987bb4a0fadb70d6defe4e0ed62e53c2637aeb
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-02-23 14:02:56 -0700

    lever prefab and anim

---
+++ MJ48-Teamwork/Assets/Scripts/FX/LeverAnim.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverAnim : MonoBehaviour
+{
+    bool isON;
+    int onValue;
+    Animator anim;
+    void Start()
+    {
+        onValue = Animator.StringToHash("LeverON");
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+
+
+    public void SetLeverState(bool state)
+    {
+        isON = state;
+        anim.SetBool(onValue, isON);
+
+    }
+
+    public bool GetLeverState()
+    {
+        return isON;
+    }
+
+}
