9210e6231306a90c58f00354b57561e2f3430720
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-02-22 23:39:50 -0700

    rune jazz + rune def scriptable obj

---
+++ MJ48-Teamwork/Assets/Scripts/FX/RuneDefinition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Rune Definition", menuName = "Rune Definition")]
+public class RuneDefinition :ScriptableObject
+{
+    public Color FollowColor;
+    public Color RetrieveColor;
+    public Color InteractColor;
+    public Color MoveColor;
+    public Color AttackColor;
+
+    public Color GetColorFromCommand(FamiliarCommands command)
+    {
+        switch (command)
+        {
+            case FamiliarCommands.FOLLOW:
+                return FollowColor;               
+            case FamiliarCommands.MOVE:
+                return MoveColor;  
+            case FamiliarCommands.ATTACK:
+                return AttackColor;
+            case FamiliarCommands.RETRIEVE:
+                return RetrieveColor;
+            case FamiliarCommands.INTERACT:
+                return InteractColor;
+            default:
+                return Color.white;
+                
+        }
+    }
+}
---
+++ MJ48-Teamwork/Assets/Scripts/FX/RuneFXController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuneFXController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] RuneDefinition runeDefinition;
+    public SpriteRenderer FXSprite;
+    
+
+    public void SpawnFXOfType(FamiliarCommands command)
+    {
+        Color runecolor = runeDefinition.GetColorFromCommand(command);
+        FXSprite.color = runecolor;
+        if(FXSprite.TryGetComponent<Animator>(out Animator a))
+        {
+            a.Play("RuneSpin");
+        }
+    }
+}
