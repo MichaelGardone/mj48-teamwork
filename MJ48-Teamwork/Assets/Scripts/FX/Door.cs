5d90fe8e7751d0370664ea8eab4123fceec57836
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-02-23 12:32:45 -0700

    animated door

---
+++ MJ48-Teamwork/Assets/Scripts/FX/Door.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool open;
+    int openValue;
+    Animator anim;
+    void Start()
+    {
+        openValue = Animator.StringToHash("Open");
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        anim.SetBool(openValue, open);
+    }
+}
