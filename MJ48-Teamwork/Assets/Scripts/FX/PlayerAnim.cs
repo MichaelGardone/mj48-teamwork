ba2115656be04affd14555f89e654da1a6ca39e7
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-02-23 18:26:11 -0700

    added visuals to player and familiar

---
+++ MJ48-Teamwork/Assets/Scripts/FX/PlayerAnim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    [Header("To change between player 1 and 2 sprites, just change the animator controller")]
+    [SerializeField] bool running;
+    private int movingHash;
+    
+    private Animator anim;
+    void Start()
+    {
+        movingHash = Animator.StringToHash("Running");
+        anim = GetComponent<Animator>();
+    }
+
+    public void SetisRunningState(bool state)
+    {
+        running = state;
+        anim.SetBool(movingHash, running);
+
+    }
+
+    public bool GetisRunningState()
+    {
+        return running;
+    }
+}
---
+++ MJ48-Teamwork/Assets/Scripts/FX/WispAnim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WispAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int attackingHash;
+    bool attacking;
+    private Animator anim;
+    void Start()
+    {
+        attackingHash = Animator.StringToHash("Attacking");
+        anim = GetComponent<Animator>();
+    }
+
+    public void SetAttackingState(bool state)
+    {
+        attacking = state;
+        anim.SetBool(attackingHash, attacking);
+
+    }
+
+    public bool GetAttackingState()
+    {
+        return attacking;
+    }
+}
